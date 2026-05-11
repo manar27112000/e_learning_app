53cd58014ba8cd76dabd2828974c85ec4e75160f
manar27112000 <adel0115225@gmail.com>
2026-05-11 23:31:02 +0300

    save user data during register

--- lib/features/auth/data/repo/auth_repo/auth_repo.dart
+++ lib/features/auth/data/repo/auth_repo/auth_repo.dart
@@ -5,17 +5,27 @@ import '../../../../../core/errors/exceptions.dart' as custom_exceptions;
 class AuthRepo {
   final SupabaseClient supabase = Supabase.instance.client;
 
-  Future<Either<String, void>> signUp({required String email, 
-  required String password,required String name}) async {
+  Future<Either<String, void>> signUp({required String email, required String password, required String name}) async {
     try {
-      await supabase.auth.signUp(
+      final res = await supabase.auth.signUp(
         email: email,
         password: password,
         data: {'name': name},
       );
 
+      if (res.user != null) {
+        final saveResult = await saveUserData(
+          id: res.user!.id,
+          name: name,
+          email: email,
+        );
 
-    
+        return saveResult.fold(
+          (error) => left("Auth created, but profile save failed: $error"),
+          (success) => right(null),
+        );
+      }
+
       return right(null);
     } on AuthException catch (e) {
       return left(e.message);
@@ -23,6 +33,7 @@ class AuthRepo {
       return left(e.toString());
     }
   }
+
 
   Future<Either<String, void>> signIn({required String email, required String password}) async {
     try {
@@ -38,4 +49,23 @@ class AuthRepo {
   Future<void> signOut() async {
     await supabase.auth.signOut();
   }
+
+Future<Either<String, void>> saveUserData({
+  required String id,
+  required String name,
+  required String email,}) async {
+  try {
+   final res= await supabase.from('users1').insert({
+    'id':id,
+    'name': name,
+    'email': email,
+   });
+   return right(null);
+  } on Exception catch (e) {
+    return left(e.toString());
+  }catch(e){
+    return left(e.toString());
+  }
+}
+
 }
